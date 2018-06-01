670b0b5302fe8fe7797fdd1612b90dee0c190edf
JoMinkyeng <minnn0609@gmail.com>
2018-06-01 21:13:20 +0900

    feat : 승인된 재능기부 게시물 목록 구현
    Model
    ---JoinService
    ---JoinServiceImpl : JoinPostListVO readDonationList(int nowPage)
    ---JoinDAO
    ---JoinDAOImpl : JoinPostListVO readDonationList(int nowPage)
    
    View
    ---readDonationList.jsp : 목록 불러와 paging 적용
    ---header.jsp : link수정
    
    Controller
    ---JoinController : String readDonationList(int nowPage, Model model)

--- heaven/src/main/java/org/kosta/heaven/model/service/JoinService.java
+++ heaven/src/main/java/org/kosta/heaven/model/service/JoinService.java
@@ -1,9 +1,11 @@
 package org.kosta.heaven.model.service;
 
+import org.kosta.heaven.model.vo.post.join.JoinPostListVO;
 import org.kosta.heaven.model.vo.post.join.JoinPostVO;
 
 public interface JoinService {
 
 	void application(JoinPostVO joinPostVO);
-
+	
+	JoinPostListVO readDonationList(int nowPage);
 }
